5fbae18d3f3fcc20981e39503b1120663999d562
David Sulton <112449977+david-sulton@users.noreply.github.com>
2022-09-21 12:28:30 -0400

    Update Combine_guests.py
     added the line:
    
    return new_list

--- Combine_guests.py
+++ Combine_guests.py
@@ -5,6 +5,8 @@ def combine_guests(guests1, guests2):
   new_list.update(guests2)
   new_list.update(guests1)
 
+  return new_list
+
 Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terry":1, "Robert":4}
 Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
 
